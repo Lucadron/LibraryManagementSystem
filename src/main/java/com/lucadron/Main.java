5d51cd83f41e3747b7e35ffac037f30c78e361d9
Lucadron <emregulsentr0@gmail.com>
2025-12-06 22:17:04 +0300

    Add i18n support

--- src/main/java/com/lucadron/Main.java
+++ src/main/java/com/lucadron/Main.java
@@ -1,9 +1,12 @@
 package com.lucadron;
 
 import com.lucadron.controller.LibraryController;
+import com.lucadron.i18n.LanguageManager;
 
 public class Main {
     public static void main(String[] args) {
+        LanguageManager.selectLanguageInteractive();
+
         LibraryController controller = new LibraryController();
         controller.start();
     }
