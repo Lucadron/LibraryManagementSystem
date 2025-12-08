b224ec1b4c818bc4b2f4c00f080fd7480301b972
Lucadron <emregulsentr0@gmail.com>
2025-12-08 23:20:37 +0300

    Database Migrator dependency has been added

--- src/main/java/com/lucadron/Main.java
+++ src/main/java/com/lucadron/Main.java
@@ -2,11 +2,19 @@ package com.lucadron;
 
 import com.lucadron.controller.LibraryController;
 import com.lucadron.i18n.LanguageManager;
+import com.lucadron.util.DatabaseMigrator;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 1) Migration'ı uygulama başlamadan önce çalıştır
+        System.out.println("🚀 Uygulama baslatiliyor. Veritabani kontrol ediliyor...");
+        DatabaseMigrator.migrate();
+
+        // 2) Dil seçimi
         LanguageManager.selectLanguageInteractive();
 
+        // 3) Konsol uygulamasını başlat
         LibraryController controller = new LibraryController();
         controller.start();
     }
